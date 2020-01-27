9fb310c8c76482e66e9a2c04f735ea325671cba7
Aiman <aimanaaw@gmail.com>
2020-01-27 16:35:15 -0500

    Added styling for components

--- ws-product-nodejs/client/my-app/src/components/eventsDaily.js
+++ ws-product-nodejs/client/my-app/src/components/eventsDaily.js
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 
 export default function eventsDaily() {
-  
+  const [dailyEvent, setDailyEvent] = useState([])
+
+
+  useEffect(() => {
+    axios.get('events/daily')
+    .then((response) => {
+      const eventData = response
+      setDailyEvent(eventData)
+
+    })
+  }, [])
+
+  return (
+    <div
+  )
 }
