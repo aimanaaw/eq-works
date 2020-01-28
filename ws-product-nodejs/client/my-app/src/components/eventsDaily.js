caec9f7a34c93f266de432f6302d54383c9a8f56
Aiman <aimanaaw@gmail.com>
2020-01-28 02:32:05 -0500

    Massaging the json data

--- ws-product-nodejs/client/my-app/src/components/eventsDaily.js
+++ ws-product-nodejs/client/my-app/src/components/eventsDaily.js
@@ -1,36 +1,41 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
-import '../styles/eventsDaily.css'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "../styles/eventsDaily.css";
 
 export default function EventsDaily() {
-  const [dailyEvent, setDailyEvent] = useState([])
-
+  const [dailyEvent, setDailyEvent] = useState([]);
 
   useEffect(() => {
-    axios.get('http://localhost:5555/events/daily')
-    .then((response) => {
-      // console.log(response.data)
-      const eventData = response.data.map(eachEvent => {
-        console.log(eachEvent)
-        return ({
-        "date": eachEvent.date,
-        "numberOfEvents": eachEvent.events
-      })})
-      console.log(eventData)
-      setDailyEvent(eventData)
-    })
-    .catch(function(error) {
-      console.log(error)
-    })
-  },[])
+    axios
+      .get("http://localhost:5555/events/daily")
+      .then(response => {
+        // console.log(response.data)
+        const eventData = response.data.map(eachEvent => {
+          return {
+            date: eachEvent.date,
+            numberOfEvents: eachEvent.events
+          };
+        });
+        console.log(eventData);
+        setDailyEvent(eventData);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }, []);
 
   return (
-    <div className='events_view'>
-      <header className='events_daily_main'>
-      {dailyEvent}
-        <h1>Welcome to React</h1>
-      </header>
-
+    <div>
+      {dailyEvent.map(event => {
+        return (
+          <div className="events_view">
+            {event.date}
+            <br />
+            {event.events}
+            <br />
+          </div>
+        );
+      })}
     </div>
-  )
+  );
 }
