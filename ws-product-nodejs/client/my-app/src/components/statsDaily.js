a4e035693ff438021e9014bb3858ed1e6bdd491c
Aiman <aimanaaw@gmail.com>
2020-01-28 02:51:00 -0500

    Adding axios get request for the stats daily component

--- ws-product-nodejs/client/my-app/src/components/statsDaily.js
+++ ws-product-nodejs/client/my-app/src/components/statsDaily.js
@@ -1,2 +1,10 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
+
+export default function StatsDaily() {
+  const [dailyStats, setDailyStats] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:5555/stats/daily")
+  })
+}
