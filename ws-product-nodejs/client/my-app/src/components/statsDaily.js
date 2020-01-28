53346e4a71e7ba9e5d2535e83bcaa7802216f25f
Aiman <aimanaaw@gmail.com>
2020-01-28 03:22:58 -0500

    stats daily component

--- ws-product-nodejs/client/my-app/src/components/statsDaily.js
+++ ws-product-nodejs/client/my-app/src/components/statsDaily.js
@@ -6,5 +6,35 @@ export default function StatsDaily() {
 
   useEffect(() => {
     axios.get("http://localhost:5555/stats/daily")
-  })
+    .then(response => {
+      const eventData = response.data.map(eachStat => {
+        return {
+          "date": eachStat.date,
+          "impressions": eachStat.impressions,
+          "clicks": eachStat.clicks,
+          "revenue": eachStat.revenue
+        };
+      });
+      console.log(eachStat);
+      setDailyStats(eachStat);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  }, []);
+
+  return (
+    <div>
+      {dailyStats.map(stat => {
+        return (
+          <div>
+            {stat.date}<br/>
+            {stat.impressions}<br/>
+            {stat.clicks}<br/>
+            {stat.revenue}<br/>
+            </div>
+        )
+      })}
+    </div>
+  )
 }
