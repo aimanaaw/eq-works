e8304154fdbace7d0b46460c50852a74b9c9c32f
Aiman <aimanaaw@gmail.com>
2020-01-28 03:49:22 -0500

    Hourly stats

--- ws-product-nodejs/client/my-app/src/components/statsHourly.js
+++ ws-product-nodejs/client/my-app/src/components/statsHourly.js
@@ -7,7 +7,26 @@ export default function StatsHourly() {
   useEffect(() => {
     axios.get("http://localhost:5555/stats/hourly")
     .then(response => {
-      const eventData = response.data.map
+      const eventData = response.data.map(eachStat => {
+        return {
+          "date": eachStat.date,
+          "hour": eachStat.hour,
+          "impressions": eachStat.impressions,
+          "clicks": eachStat.clicks,
+          "revenue": eachStat.revenue
+        };
+      });
+      console.log("Testing the hourly stats", eventData);
+      setHourlyStats(eventData);
     })
-  })
+    .catch(error => {
+      console.log(error);
+    })
+  }, []);
+
+  return (
+    <div>
+      {hourlyStats}
+    </div>
+  )
 }
