191ec0fe7668551dcdca3cfffa1833a3a86bff54
Aiman <aimanaaw@gmail.com>
2020-01-28 20:48:01 -0500

    Installed momentjs for date formats and completed the chart for events daily

--- ws-product-nodejs/client/my-app/src/components/eventsDaily.js
+++ ws-product-nodejs/client/my-app/src/components/eventsDaily.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "../styles/eventsDaily.css";
 import { Barchart, Bar, LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Legend} from 'recharts';
+import moment from 'moment';
 
 
 
@@ -15,12 +16,13 @@ export default function EventsDaily() {
       .then(response => {
         // console.log(response.data)
         const eventData = response.data.map(eachEvent => {
+          // console.log("Checking date", moment.utc(eachEvent.date).format('MM/DD/YYYY'))
           return {
-            "date": eachEvent.date,
+            "date": moment.utc(eachEvent.date).format('MM/DD/YYYY'),
             "numberOfEvents": eachEvent.events
           };
         });
-        console.log(eventData);
+        console.log(eventData[0].date);
         setDailyEvent(eventData);
       })
       .catch(function(error) {
@@ -29,18 +31,23 @@ export default function EventsDaily() {
   }, []);
 
   return (
+    <div>
+      <header>
+        <h1>Daily Events</h1>
+      </header>
     <div className="events_daily_chart">
-      <LineChart width={900} height={500} data={dailyEvent}
+      <LineChart width={1200} height={500} data={dailyEvent}
       margin={{ top: 5, right: 5, left: 20, bottom: 5 }} >
         <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="Dates" padding={{ left: 30, right: 30 }}/>
-        <YAxis type="number" domain={[0, 100]}/>
+        <XAxis dataKey="date" padding={{ left: 30, right: 30 }}/>
+        <YAxis type="number" domain={[0, 50]}/>
         <Tooltip />
         <Legend />
-        <Line type="monotone" dataKey="Dates" stroke="#F27EA1" />
+        <Line type="monotone" dataKey="numberOfEvents" stroke="#F27EA1" />
         {/* <Line type="monotone" dataKey="Amount spent per day" stroke="#61C7C9" /> */}
       </LineChart>
      
+    </div>
     </div>
   );
 }
