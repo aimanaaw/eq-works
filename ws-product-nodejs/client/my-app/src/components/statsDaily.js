8cb7a0699d1574cd5ea1acce7124ae3918d8a963
Aiman <aimanaaw@gmail.com>
2020-01-29 03:24:01 -0500

    Stats daily chart

--- ws-product-nodejs/client/my-app/src/components/statsDaily.js
+++ ws-product-nodejs/client/my-app/src/components/statsDaily.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "../styles/statsDaily.css";
-import { ComposedChart, Bar, Line, CartesianGrid, Area, XAxis, YAxis, Tooltip, Legend} from 'recharts';
+import { ComposedChart, Bar, Line, CartesianGrid, XAxis, YAxis, Tooltip, Legend} from 'recharts';
 import moment from 'moment';
 
 export default function StatsDaily() {
@@ -13,7 +13,7 @@ export default function StatsDaily() {
       const eventData = response.data.map(eachStat => {
         return {
           "date": moment.utc(eachStat.date).format('MM/DD/YYYY'),
-          "impressions": eachStat.impressions,
+          "impressions*100": (eachStat.impressions /100),
           "clicks": eachStat.clicks,
           "revenue": eachStat.revenue
         };
@@ -27,17 +27,24 @@ export default function StatsDaily() {
   }, []);
 
   return (
-    <div>
-      {dailyStats.map(stat => {
-        return (
-          <div>
-            {stat.date}<br/>
-            {stat.impressions}<br/>
-            {stat.clicks}<br/>
-            {stat.revenue}<br/>
-            </div>
-        )
-      })}
+<div>
+      <header>
+        <h1>Daily Stats</h1>
+      </header>
+    <div className="events_daily_chart">
+      <ComposedChart width={1200} height={500} data={dailyStats}
+      margin={{ top: 5, right: 5, left: 20, bottom: 5 }} >
+        <XAxis dataKey="date" padding={{ left: 30, right: 30 }}/>
+        <YAxis type="number" domain={[0, 15000]}/>
+        <Tooltip />
+        <Legend />
+        <CartesianGrid strokeDasharray="3 3" />
+        <Bar dataKey="revenue" barSize={20} fill="#413ea0"/>
+        <Line type="monotone" dataKey="impressions*100" stroke="#F27EA1" />
+        <Line type="monotone" dataKey="clicks" stroke="#61C7C9" />
+      </ComposedChart>
+     
+    </div>
     </div>
   )
 }
