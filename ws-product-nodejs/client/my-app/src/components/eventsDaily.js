a76de8db17ae013ab12728787911c25736f2fd45
Aiman <aimanaaw@gmail.com>
2020-01-28 20:12:30 -0500

    Creating chart data

--- ws-product-nodejs/client/my-app/src/components/eventsDaily.js
+++ ws-product-nodejs/client/my-app/src/components/eventsDaily.js
@@ -1,11 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "../styles/eventsDaily.css";
-
-
-import ReactDOM from 'react-dom';
-import * as serviceWorker from '../serviceWorker';
-
+import { Barchart, Bar, LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Legend} from 'recharts';
 
 
 
@@ -33,7 +29,17 @@ export default function EventsDaily() {
   }, []);
 
   return (
-    <div>
+    <div className="events_daily_main">
+      <LineChart width={900} height={500} data={dailyEvent}
+      margin={{ top: 5, right: 5, left: 20, bottom: 5 }} >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" padding={{ left: 30, right: 30 }}/>
+        <YAxis type="number" domain={[0, 1000]}/>
+        <Tooltip />
+        <Legend />
+        <Line type="monotone" dataKey="Average daily budget" stroke="#F27EA1" />
+        <Line type="monotone" dataKey="Amount spent per day" stroke="#61C7C9" />
+      </LineChart>
       {dailyEvent.map(event => {
         return (
           <div className="events_view">
