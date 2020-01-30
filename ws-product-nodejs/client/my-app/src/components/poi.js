b71b09627b5c057528df924bbdef0b46c26a521b
Aiman <aimanaaw@gmail.com>
2020-01-29 20:04:21 -0500

    Modifying the sql query to join the tables for the searchable poi table

--- ws-product-nodejs/client/my-app/src/components/poi.js
+++ ws-product-nodejs/client/my-app/src/components/poi.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import "../styles/poi.css";
+import {MDBDataTable} from 'mdbreact';
 
 
 export default function Poi() {
@@ -9,7 +11,7 @@ export default function Poi() {
     axios
     .get("http://localhost:5555/poi")
     .then(response => {
-      const poiData = response.data.map(eachPoi => {
+      const rows = response.data.map(eachPoi => {
         return {
           "poi_id": eachPoi.poi_id,
           "name": eachPoi.name,
@@ -17,8 +19,38 @@ export default function Poi() {
           "lon": eachPoi.lon
         };
       });
-      console.log("POI", poiData);
-      setPointOfInterest(poiData);
+      const columns = [
+        {
+          label: 'poi_id',
+          field: 'poi_id',
+          sort: 'asc',
+          width: 150
+        },
+        {
+          label: 'Name',
+          field: 'name',
+          sort: 'asc',
+          width: 270
+        },
+        {
+          label: 'Latitude',
+          field: 'lat',
+          sort: 'asc',
+          width: 150
+        },
+        {
+          label: 'Longitude',
+          field: 'lon',
+          sort: 'asc',
+          width: 150
+        }
+      ];
+      const tableData = {
+        "columns": columns,
+        "rows": rows
+      }
+      console.log("POI", tableData);
+      setPointOfInterest(tableData);
     })
     .catch((error) => {
       console.log(error);
@@ -27,14 +59,13 @@ export default function Poi() {
 
   return (
     <div>
-      <h1>Points of Interest</h1>
-      {pointOfInterest.map(eachPoi => {
-        return (
-          <div>
-            {eachPoi.name}
-            </div>
-        )
-      })}
+      <h1 className="poi_header">Points of Interest</h1>
+      <MDBDataTable
+      striped
+      bordered
+      hover
+      data={pointOfInterest}
+      />
     </div>
   )
 }
