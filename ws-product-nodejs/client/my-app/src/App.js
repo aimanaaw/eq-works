e49b41045c93b617f93cc5ec24ebf7a8621bd811
Aiman <aimanaaw@gmail.com>
2020-02-02 01:31:00 -0500

    Fixes

--- ws-product-nodejs/client/my-app/src/App.js
+++ ws-product-nodejs/client/my-app/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import './styles/homepageButtons.css';
-import Navbar from './components/Navbar'
+import Navbar from './components/navbar'
 
 
 function App() {
